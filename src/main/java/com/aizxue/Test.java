a5d9499a8450285e08c89e5f7532bada46a8c539
jiangsichao <393287433@qq.com>
2022-06-28 15:59:58 +0800

    ##

--- src/main/java/com/aizxue/Test.java
+++ src/main/java/com/aizxue/Test.java
@@ -1,13 +1,6 @@
 package com.aizxue;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class Test {
-public static void main(String[] args) throws Exception {
-	String url = "jdbc:sqlite:resources/test.db";
-	Connection conn =  DriverManager.getConnection(url);
-	System.out.println("连接OK");
-	conn.close();
-}
+	public static void main(String[] args) throws Exception {
+	}
 }
