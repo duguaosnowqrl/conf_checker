cf0495134fce10e91b3e00e0e440a9a1ea939283
jiangsichao <393287433@qq.com>
2022-07-05 20:34:34 +0800

    ##

--- src/main/java/com/aizxue/Test.java
+++ src/main/java/com/aizxue/Test.java
@@ -1,6 +1,21 @@
 package com.aizxue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.util.Arrays;
+
+import com.aizxue.commons.io.ByteBuffer;
+
 public class Test {
 	public static void main(String[] args) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(baos);
+		DataOutput out = dos;
+		out.writeUTF("哈哈不是ad防守打法");
+		System.out.println(Arrays.toString(baos.toByteArray()));
+		ByteBuffer buffer = new ByteBuffer();
+		buffer.writeString("哈哈不是ad防守打法");
+		System.out.println(Arrays.toString(buffer.toByteArray()));
 	}
 }
